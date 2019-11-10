4119cb600e4533c6ed95969103cc8d8e08157675
grimoh <dev@grimoh.net>
2019-11-10 13:05:20 +0900

    Update server.go

--- server/server.go
+++ server/server.go
@@ -17,9 +17,34 @@ func New() *Server {
 }
 
 func (s *Server) Handler() http.Handler {
+	s.engine.GET("/healthz", s.Healthz)
+	s.engine.GET("/readyz", s.Readyz)
 	s.engine.GET("/tests", s.ListTests)
 
 	return s.engine
+}
+
+func (s *Server) Healthz(c *gin.Context) {
+	c.String(200, "ok")
+	return
+}
+
+func (s *Server) Readyz(c *gin.Context) {
+	mes := ""
+	if !isDataLoaded() {
+		mes += "Initial data is not loaded."
+	}
+
+	if len(mes) > 0 {
+		c.String(503, mes)
+	} else {
+		c.String(200, "ok")
+	}
+	return
+}
+
+func isDataLoaded() bool {
+	return true
 }
 
 func (s *Server) ListTests(c *gin.Context) {
